8daf555eb23fcbce602e773637e91ae7567714ac
milrem <cris.a.neves@gmail.com>
2023-08-07 19:31:39 -0300

    Encapsulando os atributos e adicionando getters e setters padrões

--- src/main/java/br/com/ada/model/Endereco.java
+++ src/main/java/br/com/ada/model/Endereco.java
@@ -3,12 +3,76 @@ package br.com.ada.model;
 import br.com.ada.enums.TipoEndereco;
 
 public class Endereco {
-    TipoEndereco tipoEndereco;
-    String pais;
-    String cep;
-    String logradouro;
-    String cidade;
-    String estado;
-    String bairro;
-    String complemento;
+    private TipoEndereco tipoEndereco;
+    private String pais;
+    private String cep;
+    private String logradouro;
+    private String cidade;
+    private String estado;
+    private String bairro;
+    private String complemento;
+
+    public TipoEndereco getTipoEndereco() {
+        return tipoEndereco;
+    }
+
+    public void setTipoEndereco(TipoEndereco tipoEndereco) {
+        this.tipoEndereco = tipoEndereco;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
 }
--- src/main/java/br/com/ada/model/Telefone.java
+++ src/main/java/br/com/ada/model/Telefone.java
@@ -3,10 +3,58 @@ package br.com.ada.model;
 import br.com.ada.enums.TipoTelefone;
 
 public class Telefone {
-    TipoTelefone tipoTelefone;
-    String ddi;
-    String ddd;
-    String numero;
-    String ramal;
-    String contato;
+    private TipoTelefone tipoTelefone;
+    private String ddi;
+    private String ddd;
+    private String numero;
+    private String ramal;
+    private String contato;
+
+    public TipoTelefone getTipoTelefone() {
+        return tipoTelefone;
+    }
+
+    public void setTipoTelefone(TipoTelefone tipoTelefone) {
+        this.tipoTelefone = tipoTelefone;
+    }
+
+    public String getDdi() {
+        return ddi;
+    }
+
+    public void setDdi(String ddi) {
+        this.ddi = ddi;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getRamal() {
+        return ramal;
+    }
+
+    public void setRamal(String ramal) {
+        this.ramal = ramal;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
 }
