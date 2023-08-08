f644ae2f76d1cb64bb922199c87cb0ede59cc87d
milrem <cris.a.neves@gmail.com>
2023-08-07 22:31:26 -0300

    Executando o método de listagem de contatos da agenda e testando a exibição da lista paginada

--- src/main/java/br/com/ada/Main.java
+++ src/main/java/br/com/ada/Main.java
@@ -1,7 +1,37 @@
 package br.com.ada;
 
+import br.com.ada.enums.TipoContato;
+import br.com.ada.model.Contato;
+import br.com.ada.ui.BasicUI;
+import br.com.ada.ui.PagedListUI;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Agenda agenda = new Agenda();
+        for (int i = 0; i < 10; i++) {
+            Contato contato = new Contato();
+            contato.setTipoContato(TipoContato.PESSOAL);
+            contato.setNome("Nome "+ i);
+            contato.setSobrenome("Teste");
+            agenda.adicionarContato(contato);
+        }
+
+        System.out.println("Pagina 1");
+        List<Contato> pagina1 = agenda.listarContatos(1, 4);
+        for (int i = 0; i < pagina1.size(); i++) {
+            System.out.println(pagina1.get(i).getNomeCompleto());
+        }
+        System.out.println("Pagina 2");
+        List<Contato> pagina2 = agenda.listarContatos(2, 4);
+        for (int i = 0; i < pagina2.size(); i++) {
+            System.out.println(pagina2.get(i).getNomeCompleto());
+        }
+        System.out.println("Pagina 3");
+        List<Contato> pagina3 = agenda.listarContatos(3, 4);
+        for (int i = 0; i < pagina3.size(); i++) {
+            System.out.println(pagina3.get(i).getNomeCompleto());
+        }
     }
 }
