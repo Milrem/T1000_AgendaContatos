de1389a1717229b6395362ddea0c36760de988e4
milrem <cris.a.neves@gmail.com>
2023-08-07 22:36:06 -0300

    Utilizando a BasicUI para exibir a interface básica do usuário no console e preparar a exibição real do conteúdo da Agenda

--- src/main/java/br/com/ada/Main.java
+++ src/main/java/br/com/ada/Main.java
@@ -18,20 +18,23 @@ public class Main {
             agenda.adicionarContato(contato);
         }
 
-        System.out.println("Pagina 1");
-        List<Contato> pagina1 = agenda.listarContatos(1, 4);
-        for (int i = 0; i < pagina1.size(); i++) {
-            System.out.println(pagina1.get(i).getNomeCompleto());
-        }
-        System.out.println("Pagina 2");
-        List<Contato> pagina2 = agenda.listarContatos(2, 4);
-        for (int i = 0; i < pagina2.size(); i++) {
-            System.out.println(pagina2.get(i).getNomeCompleto());
-        }
-        System.out.println("Pagina 3");
-        List<Contato> pagina3 = agenda.listarContatos(3, 4);
-        for (int i = 0; i < pagina3.size(); i++) {
-            System.out.println(pagina3.get(i).getNomeCompleto());
-        }
+        BasicUI ui = new PagedListUI("Agenda de Contatos");
+        ui.show();
+
+//        System.out.println("Pagina 1");
+//        List<Contato> pagina1 = agenda.listarContatos(1, 4);
+//        for (int i = 0; i < pagina1.size(); i++) {
+//            System.out.println(pagina1.get(i).getNomeCompleto());
+//        }
+//        System.out.println("Pagina 2");
+//        List<Contato> pagina2 = agenda.listarContatos(2, 4);
+//        for (int i = 0; i < pagina2.size(); i++) {
+//            System.out.println(pagina2.get(i).getNomeCompleto());
+//        }
+//        System.out.println("Pagina 3");
+//        List<Contato> pagina3 = agenda.listarContatos(3, 4);
+//        for (int i = 0; i < pagina3.size(); i++) {
+//            System.out.println(pagina3.get(i).getNomeCompleto());
+//        }
     }
 }
