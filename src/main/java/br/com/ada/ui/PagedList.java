4f6a4ba9df08c0ea50a278a9d13b9b023aac1437
milrem <cris.a.neves@gmail.com>
2023-08-14 22:09:23 -0300

    Refatorada a PagedListUI para torná-la totalmente generica, para exibir qualquer listagem

--- src/main/java/br/com/ada/ui/PagedList.java
+++ src/main/java/br/com/ada/ui/PagedList.java
@@ -4,6 +4,6 @@ import br.com.ada.model.Contato;
 
 import java.util.List;
 
-public interface PagedList {
-    List<Contato> listarContatos(int pagina, int tamanhoPagina);
+public interface PagedList<T> {
+    List<T> listar(int pagina, int tamanhoPagina);
 }
