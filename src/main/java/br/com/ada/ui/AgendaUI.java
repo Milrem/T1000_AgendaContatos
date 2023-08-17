653f5afe39c880a396308f41a21681fda9ef86a5
milrem <cris.a.neves@gmail.com>
2023-08-16 21:44:18 -0300

    Completar a AgendaUI para integrar a ContatoUI as funções de gravação dos dados

--- src/main/java/br/com/ada/ui/AgendaUI.java
+++ src/main/java/br/com/ada/ui/AgendaUI.java
@@ -1,6 +1,8 @@
 package br.com.ada.ui;
 
+import br.com.ada.enums.TipoContato;
 import br.com.ada.model.Contato;
+import br.com.ada.service.AgendaService;
 
 public class AgendaUI extends PagedListUI<Contato> {
     public AgendaUI(PagedList<Contato> pageSource) {
@@ -9,20 +11,24 @@ public class AgendaUI extends PagedListUI<Contato> {
 
     @Override
     protected void showItem(Contato item) {
-        ContatoUI ui = new ContatoUI("", item, new EditItemCallback<Contato>() {
+        ContatoUI ui = new ContatoUI("Contato", item, new EditItemCallback<Contato>() {
             @Override
-            public void remove(Contato item) {
-
+            public void remove(Contato ref) {
+                AgendaService.getInstance().del(ref);
             }
 
             @Override
             public void add(Contato item) {
-
+                AgendaService.getInstance().add(item);
             }
 
             @Override
-            public boolean isNew(Contato item) {
-                return false;
+            public boolean isNew(Contato ref) {
+                if (ref.equals(item)) {
+                    return false;
+                } else {
+                    return true;
+                }
             }
         });
         ui.show();
@@ -30,6 +36,9 @@ public class AgendaUI extends PagedListUI<Contato> {
 
     @Override
     protected void addItem() {
-
+        Contato newContato = new Contato();
+        newContato.setTipoContato(TipoContato.PESSOAL);
+        AgendaService.getInstance().add(newContato);
+        showItem(newContato);
     }
 }
