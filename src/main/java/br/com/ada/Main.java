9ee3c6b6226e59d56a80828767c1d0700db82d12
milrem <cris.a.neves@gmail.com>
2023-08-16 21:45:08 -0300

    Alterando o main para fazer a carga de contatos da agenda usando o AgendaService e salvar as alterações ao fechar a aplicação

--- src/main/java/br/com/ada/Main.java
+++ src/main/java/br/com/ada/Main.java
@@ -2,21 +2,26 @@ package br.com.ada;
 
 import br.com.ada.enums.TipoContato;
 import br.com.ada.model.Contato;
+import br.com.ada.service.AgendaService;
 import br.com.ada.ui.AgendaUI;
 import br.com.ada.ui.BasicUI;
 
 public class Main {
     public static void main(String[] args) {
-        Agenda agenda = new Agenda();
-        for (int i = 0; i < 10; i++) {
-            Contato contato = new Contato();
-            contato.setTipoContato(TipoContato.PESSOAL);
-            contato.setNome("Nome "+ i);
-            contato.setSobrenome("Teste");
-            agenda.adicionarContato(contato);
-        }
+        AgendaService agendaService = AgendaService.getInstance();
+//        for (int i = 0; i < 10; i++) {
+//            Contato contato = new Contato();
+//            contato.setTipoContato(TipoContato.PESSOAL);
+//            contato.setNome("Nome "+ i);
+//            contato.setSobrenome("Teste");
+//            agendaService.add(contato);
+//        }
+//        agendaService.exportar("contatos.dat", true);
+        agendaService.importar("contatos.dat");
 
-        BasicUI ui = new AgendaUI(agenda);
+        BasicUI ui = new AgendaUI(new Agenda());
         ui.show();
+
+        agendaService.exportar("contatos.dat", true);
     }
 }
